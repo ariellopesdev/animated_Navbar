cd8818fcc4e090c92c44d91b9e6eb7d89e9e4ebc
lubyelcuritiba <lubyel.lopes@gmail.com>
2022-11-18 23:48:09 -0300

    Finalized

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+    define('INCLUDE_PATH','http://localhost/animated_Navbar/');//url amigável
+?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('config.php'); ?>
+<html>
+
+<head>
+    <title>Animated Navbar</title>
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>fonts/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH; ?>style.css" rel="stylesheet" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave, do, meu, site">
+    <meta name="description" content="Descrição do meu website">
+    <meta name="author" content="Ariel Lopes">
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Sobre</a>
+        <a href="#">Blog</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Contato</a>
+        <div class="animation start-home"></div>
+    </nav>
+</body>
+
+</html>
